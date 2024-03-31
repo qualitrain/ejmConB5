515341ab2afb4deed448fc44652d4fd5a496020c
Alex Cruz <acruz@qualitrain.com.mx>
2024-03-31 15:18:51 -0600

    Corrección de import de ruteo en caja-ruteo

--- src/app/demo-ruteo/caja-ruteo/caja-ruteo.component.ts
+++ src/app/demo-ruteo/caja-ruteo/caja-ruteo.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-caja-ruteo',
   standalone: true,
   imports: [
     CommonModule,
-    RouterOutlet
+    RouterModule
   ],
   templateUrl: './caja-ruteo.component.html',
   styleUrl: './caja-ruteo.component.css'
